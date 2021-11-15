8c2114eb9cfe0c987870ea5c6c7c8227493dcb5c
snehakweera77 <snehakweera77@gmail.com>
2021-11-15 19:07:41 +0530

    messages and conversations routes added

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
 
 dotenv.config();
 mongoose
@@ -20,6 +22,8 @@ app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
